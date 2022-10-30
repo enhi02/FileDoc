1c49fc1076cdd5b5374564537a6fa73c978e7818
hienpt9602@gmail.com <hienpt9602@gmail.com>
2022-10-30 18:56:50 +0700

    fix

--- FileDoc/Startup.cs
+++ FileDoc/Startup.cs
@@ -47,28 +47,28 @@ namespace FileDoc
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FileDoc", Version = "v1" });
-                //var jwtSecurityScheme = new OpenApiSecurityScheme
-                //{
-                //    BearerFormat = "JWT",
-                //    Name = "JWT Authentication",
-                //    In = ParameterLocation.Header,
-                //    Type = SecuritySchemeType.Http,
-                //    Scheme = JwtBearerDefaults.AuthenticationScheme,
-                //    Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+                var jwtSecurityScheme = new OpenApiSecurityScheme
+                {
+                    BearerFormat = "JWT",
+                    Name = "JWT Authentication",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                    Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
 
-                //    Reference = new OpenApiReference
-                //    {
-                //        Id = JwtBearerDefaults.AuthenticationScheme,
-                //        Type = ReferenceType.SecurityScheme
-                //    }
-                //};
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
 
-                //c.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+                c.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
 
-                //c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                //{
-                //    { jwtSecurityScheme, Array.Empty<string>() }
-                //});
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { jwtSecurityScheme, Array.Empty<string>() }
+                });
             });
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
